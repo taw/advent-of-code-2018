1d35419a834a3dd59936f4d085d579bc10975760
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-12-08 10:09:27 +0000

    Day 08a

---
+++ solutions/day_08.rb
@@ -0,0 +1,44 @@
+require "pathname"
+require "pry"
+require "set"
+
+class Node
+  attr_reader :children, :metadata
+
+  def initialize(children, metadata)
+    @children = children
+    @metadata = metadata
+  end
+
+  def metadata_sum
+    metadata.sum + children.map(&:metadata_sum).sum
+  end
+
+  def self.parse(tokens)
+    num_children = tokens.shift
+    num_metadata = tokens.shift
+    children = num_children.times.map { parse(tokens) }
+    metadata = num_metadata.times.map { tokens.shift }
+    Node.new(children, metadata)
+  end
+end
+
+class Day08
+  def initialize(input_file = "")
+    @input_file = input_file
+  end
+
+  def input_path
+    Pathname("#{__dir__}/../data/input_08#{@input_file}.txt")
+  end
+
+  def data
+    @data ||= input_path.read.split.map(&:to_i)
+  end
+
+  def root_node
+    unless @root_node
+      @root_node = Node.parse(data.dup)
+    end
+  end
+end
